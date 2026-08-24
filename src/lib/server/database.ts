b6cf9b1d4a2c9ee35cb89c2b106a4fc65ae0b2c4
Erik Borowski <erik@ebinf.eu>
2026-08-24 14:04:35 +0200

    fix: wrap isFetching reset in building check

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -1,3 +1,4 @@
+import { building } from '$app/env';
 import { env } from '$env/dynamic/private';
 import { PrismaClient } from '$prisma/client';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
@@ -7,11 +8,13 @@ const connectionString = env.DATABASE_URL;
 const adapter = new PrismaBetterSqlite3({ url: connectionString });
 const prisma = new PrismaClient({ adapter });
 
-// Reset isFetching to false on server start
-await prisma.remote.updateMany({
-	data: {
-		isFetching: false
-	}
-});
+if (!building) {
+	// Reset isFetching to false on server start
+	await prisma.remote.updateMany({
+		data: {
+			isFetching: false
+		}
+	});
+}
 
 export default prisma;
